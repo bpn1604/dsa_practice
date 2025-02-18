07302ae4c4b4af7e0cfc937efa4721463a85dcd3
Bipin Mishra <98827173+bpn1604@users.noreply.github.com>
2025-02-18 14:05:12 +0530

    Create theory .js

---
+++ theory .js
@@ -0,0 +1,13 @@
+1. What is high-level design, and how does it differ from low-level design?
+
+    High-Level Design (HLD): Focuses on the overall architecture of the system, including components, modules, and their interactions. It provides a bird's-eye view of the system.
+    Low-Level Design (LLD): Deals with the implementation details of individual components, including algorithms, class diagrams, and data structures.
+
+2.Explain the key components of a high-level design document.
+
+    Architecture Diagram: Visual representation of system components and their relationships.
+    Modules and Components: Breakdown of the system into modules with defined responsibilities.
+    Data Flow: Flow of data between components.
+    Technology Stack: Technologies used for each part of the system.
+    APIs and Interfaces: Communication protocols between services.
+    Scalability and Performance Considerations: How the system handles load, caching, and scaling.
